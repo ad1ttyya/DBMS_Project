7a80091733d6faa52b4171f320d153ec3d92096b
anshoomaan <anshoomaan.c.maithani@gmail.com>
2024-04-13 11:59:49 +0530

    worked on backend and front

--- backend.py
+++ backend.py
@@ -4,3 +4,36 @@ f = open("Credentials.txt")
 mydb = mysql.connector.connect(host=f.readline(),user=f.readline(),password=f.readline())
 print(mydb)
 f.close()
+
+#to check that the import backend works
+# def hello():
+# print("dawdwadawdawdwad")
+
+mycursor = mydb.cursor()
+mycursor.execute("use USER_CREDENTIALS")
+mycursor.execute("SELECT * FROM USER_LOGIN_DATA")
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
+  
+print("\n\n\n")  
+
+mycursor.execute("use COMP_DATABASE")
+mycursor.execute("SELECT * FROM INVENTORY_ALL_ITEMS")
+myresult = mycursor.fetchall()
+for y in myresult:
+  print(y)
+  
+print("\n\n\n")   
+  
+mycursor.execute( "SELECT * FROM INVENTORY_ALL_ITEMS WHERE STATUS='IN' " )
+myresult = mycursor.fetchall()
+for z in myresult:
+  print(z)
+  
+print("\n\n\n")    
+  
+mycursor.execute( "SELECT * FROM INVENTORY_ALL_ITEMS WHERE STATUS='OUT' " )
+myresult = mycursor.fetchall()
+for m in myresult:
+  print(m)
