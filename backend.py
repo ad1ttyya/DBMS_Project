6a2c863c9f523acd247234a2154bb8931ed88e31
anshoomaan <anshoomaan.c.maithani@gmail.com>
2024-04-13 12:08:20 +0530

    worked on front and back

--- backend.py
+++ backend.py
@@ -16,24 +16,24 @@ myresult = mycursor.fetchall()
 for x in myresult:
   print(x)
   
-print("\n\n\n")  
+print("\n\n")  
 
 mycursor.execute("use COMP_DATABASE")
 mycursor.execute("SELECT * FROM INVENTORY_ALL_ITEMS")
 myresult = mycursor.fetchall()
-for y in myresult:
-  print(y)
+for x in myresult:
+  print(x)
   
-print("\n\n\n")   
+print("\n\n")   
   
 mycursor.execute( "SELECT * FROM INVENTORY_ALL_ITEMS WHERE STATUS='IN' " )
 myresult = mycursor.fetchall()
-for z in myresult:
-  print(z)
+for x in myresult:
+  print(x)
   
-print("\n\n\n")    
+print("\n\n")    
   
 mycursor.execute( "SELECT * FROM INVENTORY_ALL_ITEMS WHERE STATUS='OUT' " )
 myresult = mycursor.fetchall()
-for m in myresult:
-  print(m)
+for x in myresult:
+  print(x)
